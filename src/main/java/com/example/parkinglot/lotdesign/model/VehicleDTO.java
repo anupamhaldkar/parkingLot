f95a9b0a31fba885e05a3795a89f3d988934aec1
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-05 15:48:52 +0530

    Model entity id

--- src/main/java/com/example/parkinglot/lotdesign/model/VehicleDTO.java
+++ src/main/java/com/example/parkinglot/lotdesign/model/VehicleDTO.java
@@ -1,2 +1,10 @@
-package com.example.parkinglot.lotdesign.model;public class VehicleDTO {
+package com.example.parkinglot.lotdesign.model;
+
+import javax.persistence.Id;
+
+public class VehicleDTO {
+    @Id
+    private Long id;
+    private String vehicleNumber;
+    private String ownerNumber;
 }
