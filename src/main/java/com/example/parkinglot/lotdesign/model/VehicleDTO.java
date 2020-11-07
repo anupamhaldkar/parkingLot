e8f543287ee38fded3b03625b977dadba2dfe65d
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-07 15:51:51 +0530

    getter setter removed Lombok🏹

--- src/main/java/com/example/parkinglot/lotdesign/model/VehicleDTO.java
+++ src/main/java/com/example/parkinglot/lotdesign/model/VehicleDTO.java
@@ -1,34 +1,16 @@
 package com.example.parkinglot.lotdesign.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Id;
 
+@Getter
+@Setter
 public class VehicleDTO {
     @Id
     private Long id;
     private String vehicleNumber;
     private String ownerNumber;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getVehicleNumber() {
-        return vehicleNumber;
-    }
-
-    public void setVehicleNumber(String vehicleNumber) {
-        this.vehicleNumber = vehicleNumber;
-    }
-
-    public String getOwnerNumber() {
-        return ownerNumber;
-    }
-
-    public void setOwnerNumber(String ownerNumber) {
-        this.ownerNumber = ownerNumber;
-    }
 }
