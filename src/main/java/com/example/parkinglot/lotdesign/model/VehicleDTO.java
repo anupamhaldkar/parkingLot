e580e4418f3514336ace775008009f481a659d63
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-07 15:49:39 +0530

    getter setter

--- src/main/java/com/example/parkinglot/lotdesign/model/VehicleDTO.java
+++ src/main/java/com/example/parkinglot/lotdesign/model/VehicleDTO.java
@@ -7,4 +7,28 @@ public class VehicleDTO {
     private Long id;
     private String vehicleNumber;
     private String ownerNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public String getOwnerNumber() {
+        return ownerNumber;
+    }
+
+    public void setOwnerNumber(String ownerNumber) {
+        this.ownerNumber = ownerNumber;
+    }
 }
