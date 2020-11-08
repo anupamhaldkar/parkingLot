c21cb6d05c16ff919fb2c480221e87931fbea81e
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-08 16:03:37 +0530

    Extended

--- src/main/java/com/example/parkinglot/lotdesign/repository/ParkingRepository.java
+++ src/main/java/com/example/parkinglot/lotdesign/repository/ParkingRepository.java
@@ -1,7 +1,9 @@
 package com.example.parkinglot.lotdesign.repository;
 
+import com.example.parkinglot.lotdesign.model.VehicleDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ParkingRepository {
+public interface ParkingRepository extends JpaRepository<VehicleDTO, Long> {
 }
