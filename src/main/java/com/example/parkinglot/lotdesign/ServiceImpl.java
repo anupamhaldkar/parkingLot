09f106818311420feb88a54dc80036179f4cce6a
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-10 17:45:38 +0530

    Parking Method

--- src/main/java/com/example/parkinglot/lotdesign/ServiceImpl.java
+++ src/main/java/com/example/parkinglot/lotdesign/ServiceImpl.java
@@ -16,4 +16,15 @@ public class ServiceImpl implements ParkingService {
     public List<VehicleDTO> getAllVehicles() {
         return parkingRepository.findAll();
     }
+
+    @Override
+    public String park(VehicleDTO vehicleDTO) {
+        if(vehicleDTO.getId() >0 && vehicleDTO.getId() < 11) {
+            parkingRepository.save(vehicleDTO);
+        }
+        else {
+            throw new RuntimeException("Kindly provide id between 1 to 10");
+        }
+        return "Parked";
+    }
 }
--- src/main/java/com/example/parkinglot/lotdesign/service/ParkingService.java
+++ src/main/java/com/example/parkinglot/lotdesign/service/ParkingService.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface ParkingService {
     List<VehicleDTO> getAllVehicles();
+    String park(VehicleDTO vehicleDTO);
 }
