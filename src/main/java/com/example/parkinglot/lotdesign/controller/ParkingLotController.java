e05294c91a3bd518acac4fa50e653c275fc2dffa
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-12 18:41:41 +0530

    PostMapping 🗾

--- src/main/java/com/example/parkinglot/lotdesign/controller/ParkingLotController.java
+++ src/main/java/com/example/parkinglot/lotdesign/controller/ParkingLotController.java
@@ -4,6 +4,7 @@ import com.example.parkinglot.lotdesign.model.VehicleDTO;
 import com.example.parkinglot.lotdesign.service.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class ParkingLotController {
     @GetMapping("/parkinginfo")
     public List<VehicleDTO> getAllVehicles(){
         return parkingService.getAllVehicles();
+    }
+
+    @PostMapping("/park")
+    public String park(VehicleDTO vehicleDTO){
+        return parkingService.park(vehicleDTO);
     }
 }
