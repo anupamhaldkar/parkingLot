77a7b3159654e4f43beeffd1152bbedc47dd78ce
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-12 18:39:49 +0530

    GetMapping 🗾

--- src/main/java/com/example/parkinglot/lotdesign/controller/ParkingLotController.java
+++ src/main/java/com/example/parkinglot/lotdesign/controller/ParkingLotController.java
@@ -1,4 +1,22 @@
 package com.example.parkinglot.lotdesign.controller;
 
+import com.example.parkinglot.lotdesign.model.VehicleDTO;
+import com.example.parkinglot.lotdesign.service.ParkingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parkinglot")
 public class ParkingLotController {
+    @Autowired
+    ParkingService parkingService;
+
+    @GetMapping("/parkinginfo")
+    public List<VehicleDTO> getAllVehicles(){
+        return parkingService.getAllVehicles();
+    }
 }
