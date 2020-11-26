515b558256919b1399ba8530936760edaf55c709
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-26 15:05:19 +0530

    Parking lot initializer

---
+++ src/main/java/com/example/parkinglot/lotdesign/LotDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkinglot.lotdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotDesignApplication.class, args);
+	}
+
+}
